44775acab1ee82cb179e5e3026c5224cd02113f8
Sanjaych4 <sanjayb292@gmail.com>
2023-03-22 16:17:39 +0530

    Day38 added

--- Day38 Text Editor/texteditor.py
+++ Day38 Text Editor/texteditor.py
@@ -2,85 +2,93 @@
 
 import tkinter as tk
 from tkinter import filedialog
+from tkinter import messagebox
 
 class TextEditor:
+
     def __init__(self, master):
         self.master = master
         self.master.title("Text Editor")
-        self.master.geometry("500x500")
-        
-        # create a menu bar
+
+        self.textarea = tk.Text(self.master, wrap=tk.WORD)
+        self.textarea.pack(side=tk.LEFT, fill=tk.BOTH, expand=True)
+
+        self.scrollbar = tk.Scrollbar(self.master, orient=tk.VERTICAL, command=self.textarea.yview)
+        self.scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+
+        self.textarea.configure(yscrollcommand=self.scrollbar.set)
+
+        self.create_menu()
+
+    def create_menu(self):
         menubar = tk.Menu(self.master)
-        
-        # create a file menu
-        file_menu = tk.Menu(menubar, tearoff=0)
+        self.master.config(menu=menubar)
+
+        file_menu = tk.Menu(menubar, tearoff=False)
         file_menu.add_command(label="New", command=self.new_file)
         file_menu.add_command(label="Open", command=self.open_file)
         file_menu.add_command(label="Save", command=self.save_file)
-        file_menu.add_separator()
-        file_menu.add_command(label="Exit", command=self.master.quit)
+        file_menu.add_command(label="Save As", command=self.save_file_as)
         menubar.add_cascade(label="File", menu=file_menu)
-        
-        # create an edit menu
-        edit_menu = tk.Menu(menubar, tearoff=0)
-        edit_menu.add_command(label="Cut", command=self.cut)
-        edit_menu.add_command(label="Copy", command=self.copy)
-        edit_menu.add_command(label="Paste", command=self.paste)
-        menubar.add_cascade(label="Edit", menu=edit_menu)
-        
-        # create a view menu
-        view_menu = tk.Menu(menubar, tearoff=0)
+
+        view_menu = tk.Menu(menubar, tearoff=False)
         view_menu.add_command(label="Zoom In", command=self.zoom_in)
         view_menu.add_command(label="Zoom Out", command=self.zoom_out)
         menubar.add_cascade(label="View", menu=view_menu)
-        
-        # set the menu bar
-        self.master.config(menu=menubar)
-        
-        # create a text widget
-        self.text = tk.Text(self.master)
-        self.text.pack(expand=True, fill="both")
-    
+
+        mode_menu = tk.Menu(menubar, tearoff=False)
+        mode_menu.add_command(label="Light Mode", command=self.light_mode)
+        mode_menu.add_command(label="Dark Mode", command=self.dark_mode)
+        menubar.add_cascade(label="Mode", menu=mode_menu)
+
     def new_file(self):
-        self.text.delete(1.0, tk.END)
-    
+        self.textarea.delete("1.0", tk.END)
+
     def open_file(self):
         file_path = filedialog.askopenfilename()
         if file_path:
             with open(file_path, "r") as file:
                 text = file.read()
-                self.text.delete(1.0, tk.END)
-                self.text.insert(tk.END, text)
-    
+                self.textarea.delete("1.0", tk.END)
+                self.textarea.insert(tk.END, text)
+
     def save_file(self):
-        file_path = filedialog.asksaveasfilename()
+        try:
+            file = open(self.file_path, "w")
+            text = self.textarea.get("1.0", tk.END)
+            file.write(text)
+            file.close()
+        except AttributeError:
+            self.save_file_as()
+
+    def save_file_as(self):
+        file_path = filedialog.asksaveasfilename(defaultextension=".txt")
         if file_path:
-            with open(file_path, "w") as file:
-                text = self.text.get(1.0, tk.END)
-                file.write(text)
-    
-    def cut(self):
-        self.text.event_generate("<<Cut>>")
-    
-    def copy(self):
-        self.text.event_generate("<<Copy>>")
-    
-    def paste(self):
-        self.text.event_generate("<<Paste>>")
-    
+            self.file_path = file_path
+            self.save_file()
+
     def zoom_in(self):
-        font_size = self.text.cget("font").split(" ")[-1]
-        new_size = int(font_size) + 2
-        self.text.configure(font=("TkDefaultFont", new_size))
-    
+        font_size = int(self.textarea["font"].split()[1])
+        font_size += 2
+        self.textarea.configure(font=("TkDefaultFont", font_size))
+
     def zoom_out(self):
-        font_size = self.text.cget("font").split(" ")[-1]
-        new_size = int(font_size) - 2
-        if new_size > 0:
-            self.text.configure(font=("TkDefaultFont", new_size))
+        font_size = int(self.textarea["font"].split()[1])
+        font_size -= 2
+        if font_size > 0:
+            self.textarea.configure(font=("TkDefaultFont", font_size))
+
+    def light_mode(self):
+        self.master.configure(bg="white")
+        self.textarea.configure(bg="white", fg="black")
+
+    def dark_mode(self):
+        self.master.configure(bg="black")
+        self.textarea.configure(bg="black", fg="white")
 
 if __name__ == "__main__":
     root = tk.Tk()
-    TextEditor(root)
+    editor = TextEditor(root)
     root.mainloop()
+
 
